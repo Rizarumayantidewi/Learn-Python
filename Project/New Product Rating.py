eb5cda7d539790cf7d21c068a8fd1e3bb8aec2f4
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-09-23 13:31:03 +0700

    Organizing Data 1

---
+++ Project/New Product Rating.py
@@ -0,0 +1,11 @@
+flavors = ["apple pie", "yogurt", "beef teriyaki", "Strawberry juice"]
+print("New flavors:")
+print(flavors)
+
+ratings = [8, 8.5, 9, 8.8]
+print("Consumer ratings:")
+print(ratings)
+
+passed = [True, False, True, False]
+print("Release:")
+print(passed)
