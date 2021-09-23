89c5325f72c493ca965daf14d6091d7e9c6b39de
Rizarumayantidewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-09-23 11:34:53 +0700

    Types And Comparisons 2

---
+++ Project/Survey Data Insights.py
@@ -0,0 +1,10 @@
+frequency = “one a week”
+Intensity = “low”
+
+highly_active = frequency == “daily”
+print(“Highly active user:”)
+print(highly_active)
+
+high_intensity = intensity == “high”
+print(“High intensity sports:”)
+print(high_intensity)
