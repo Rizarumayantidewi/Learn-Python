a9bad40a31b75ed42c1459a04fb80c370b376e54
Rizarumayantidewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-09-23 11:08:11 +0700

    Python Basics

---
+++ 1. Creating Variables.py
@@ -0,0 +1,51 @@
+#No matter how complex a program is, it begins with a single line of code. This first line is usually a variable.
+#Programs use variables to remember information. Like moving boxes, variables have content and names that tell us what's inside.
+#1) Names
+#To create a variable, we need to give it a name. Variable names need to be single words and therefore have no spaces. example :
+home
+
+#If we want a variable name with multiple words, we use snake case. Snake case means using _ to connect the additional words. example :
+my_home
+
+#To help us understand what's inside a variable we pick descriptive names. Example:
+Home_city_province
+
+
+#2) Values
+#variable names can contain numbers, too. Adding numbers is useful for multiple similar variables. example:
+hotel_1
+
+#After creating and naming a variable, we use the = sign to store a value inside it. example :
+hotel = "Dariza"
+
+#When we store a value in a variable, it's like putting stuff inside a box. Example:
+My_hotel = “Dariza”
+
+#The values we've been storing, like "Riza" are strings. Strings are words in double quotes. Example:
+“Riza”
+
+#Strings can contain all sorts of letters and symbols, including spaces, example:
+"Riza Rumayanti Dewi"
+
+
+#3)Console
+#Lines of code are instructions for the computer to follow. when we run code, we tell the computer to follow the instructions we put together.
+
+#The order of the instructions matters because the computer follows the instructions line by line, Example:
+Step_1 = “Like”
+Step_2 = “Dear”
+Step_3 = “Love”
+	
+#with the special instruction print(), we tell the computer to display a value in an area called the console, also known as the shell. Example:
+print(“Salut!”)
+
+#we can use the print() instruction as often as we want. The computer displays every value on a line in the console. Example:
+print(“дорогая Риза”)
+
+#we can use print() to display variables. Example:
+greeting = “Hai”
+print(greeting)
+
+#when we display variables in the console, their values appear instead of their names.if we print name here, it'll show its value. Example:
+name = Riza
+print(name)
