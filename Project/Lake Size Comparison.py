c4258519f8fa713cae8c86f2c756eb88c7ef5793
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-09-24 14:54:26 +0700

    Using Lists 1

---
+++ Project/Lake Size Comparison.py
@@ -0,0 +1,16 @@
+sizes = [8000, 25000, 11000, 25118, 10221, 20210]
+
+largest = max(sizes)
+
+print("Largest lake in sq mi:")
+print(largest)
+
+smallest = min(sizes)
+
+print("11th largest lake in sq mi:")
+print(smallest)
+
+difference = largest - smallest
+
+print("Difference between lakes in sq mi:")
+print(difference)
