b58a1ecde15f39be8a70a525122d173e5e18990e
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-09-24 14:50:41 +0700

    Organizing Data 2

---
+++ Project/Stocks Tracker.py
@@ -0,0 +1,12 @@
+strawberry_stocks = [200, 125, 881]
+
+strawberry_stocks[0] = 225
+print("latest value:")
+print(strawberry_stocks[0])
+
+strawberry_stocks[1] = 118
+print("highest value:")
+print(strawberry_stocks[1])
+
+print("lowest value:")
+print(strawberry_stocks[2])
