82f27ee38be21837a75a3b224c7291922026a48e
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-09-24 21:59:11 +0700

    Using Lists 2

---
+++ Project/Customer Service Queue System.py
@@ -0,0 +1,18 @@
+from typing import Counter
+
+
+customers = ["Riza", "Gilang", "Kai", "Rumayanti", "Permana"]
+
+free_counters = [11, 8, 14, 25, 1]
+
+customers.sort()
+
+free_counters.sort()
+
+customer = customers[0]
+
+counter = free_counters[0]
+
+print(customer)
+print("Visit counter:")
+print(counter)
