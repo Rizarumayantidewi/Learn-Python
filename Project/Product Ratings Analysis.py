d1181705312a8de89c2cd9eed3523c0981da95f2
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-09-24 22:00:48 +0700

    Using Lists 4

---
+++ Project/Product Ratings Analysis.py
@@ -0,0 +1,13 @@
+ratings = [1, 5, 2, 1, 4, 2, 2, 3, 5]
+
+five_star = ratings.count(5)
+
+one_star = ratings.count(1)
+
+amounts = [five_star, one_star]
+
+print("Five star ratings:")
+print(amounts[0])
+
+print("One star ratings:")
+print(amounts[1])
