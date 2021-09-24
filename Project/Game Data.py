b72d74e138b98a35622fc3d0de2e47e88e0701bd
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-09-25 06:43:36 +0700

    Using Strings 3

---
+++ Project/Game Data.py
@@ -0,0 +1,20 @@
+player_1 = "Riza"
+player_2 = "Gilang"
+current_round = 1
+
+print("Game on!")
+print(f"player 1: {player_1}")
+print(f"player 2: {player_2}")
+print("--------------------")
+
+print(f"Round {current_round}!")
+player_1_score = 11
+player_2_score = 8
+print(f"{player_1} wins with {player_1_score} to {player_2_score}")
+print("--------------------")
+
+print(f"Round {current_round + 1}!")
+player_1_score = 25
+player_2_score = 88
+print(f"{player_2} wins with {player_2_score} to {player_1_score}")
+print("--------------------")
