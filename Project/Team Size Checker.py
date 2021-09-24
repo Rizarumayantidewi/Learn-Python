de2a90645aa54d2ceff21f496a6a1fe04d600a64
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-09-24 22:00:04 +0700

    Using Lists 3

---
+++ Project/Team Size Checker.py
@@ -0,0 +1,17 @@
+team_1 = ["Riza", "Rumayanti", "Dewi"]
+team_2 = ["Gilang", "Permana", "Kai"]
+
+size_1 = len(team_1)
+size_2 = len(team_2)
+
+if size_1 != size_2:
+    print("Teams must have the same size!")
+else:
+    print("Game on!")
+
+if size_1 == 11
+    print("Rounds left: 11")
+elif size_1 == 8
+    print("Rounds left: 8")
+else
+    print("Final round!")
