c96c41bdf5dc8d86be82956210049a799689a13c
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-09-25 07:12:05 +0700

    Delete Transforming DNA to RNA.py

--- Transforming DNA to RNA.py
+++
@@ -1,7 +0,0 @@
-Sequence = "tatgctttcc#tataggtctg#ctattcaatg"
-dna_list = Sequence.split("#")
-print(dna_list)
-
-for dna in dna_list:
-    rna = dna.replace("t", "u")
-    print(f"DNA: {dna} -> RNA: {rna}")
