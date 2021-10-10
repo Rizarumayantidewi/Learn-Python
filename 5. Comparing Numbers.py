211ca337a46a7183089b58b8ef1ea59cfdd3eb15
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-10-10 09:40:33 +0700

    Types and Comparisons 1

---
+++ 5. Comparing Numbers.py
@@ -0,0 +1,38 @@
+#1)	We can use comparisons to check if a numbers is less than or greater than another number. 
+#To check if a number is less than another number, we use the less-than operator < ,example:
+8 < 11
+
+#if the number on the left is less than the number on the right, like in 8 < 11 , the result is True ,example:
+print(8 < 11)
+
+#if the number on the left isn’t  less than the number on the right, like in 8 < 1 , the result is False ,example:
+print(8 < 1)
+
+#To check if a number is greater than another number, we use the greater-than operator > ,example:
+Print(25 > 11)
+
+
+#2)	To check if a number is less than or equal to another number, we use the less-than-or-to operator <= ,example:
+Print(8 <= 11)
+
+#if the number on the left is less than or equal to the number on the right, like in 11 <= 11 , the result is True ,example:
+print(11 <= 11)
+
+#To check if a number is greater than or equal to another number, we use the greater-than-or-equal-to operator >= ,example:
+Print(8 >= 8)
+
+#To store the result of a comparison in a variable, we use the = sign. Example:
+Result = 8 <= 11
+Print(result)
+
+#We can also use a comparison operator to compare a variable with another variable, like in min <= max ,example:
+Min = 1
+Max = 10
+Result = min <= max
+Print(result)
+
+#Check if battery_level is less than or equal to 11 with the less-than-or-equal-to operator <= ,example:
+Battery_level = 8
+Low = battery_level <= 11
+Print(“Low battery:”)
+Print(low)
