eef0d9b855219979d7f7313d24bfdfca7a8492c3
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-10-12 12:01:30 +0700

    Types and Comparisons 2

---
+++ 6. Comparing Strings.py
@@ -0,0 +1,37 @@
+#1)	We can use comparisons to check if a string is equal to or not equal to another string. Example:
+Print(“online” == “online”)
+Print(“online” != “offline”)
+
+#To check if a string is equal to another string, we also use the equality operator == ,example:
+Print(“grape” == “grape”)
+
+#If the string on the left is equal to string on the right, as in “banana” == “banana” , the result is True ,example:
+Print(“banana” == “banana”)
+
+#If the string on the left isn’t equal to the string on the right, as in “banana” == “grape” , the result is False ,example:
+Print(“banana” == “grape”)
+
+#We can also compare variables that store strings with each other, like in fruit_1 == fruit_2 ,example:
+fruit_1 = “banana”
+fruit_2 = “grape”
+print(fruit_1 == fruit_2)
+
+
+#2)	to check if a string isn’t equal to another string, we use the inequality operator != ,example:
+print(“Riza” != “Gilang”)
+
+#If the left string isn’t equal to the right string, as in “Riza” != “Gilang” , the result is True ,example:
+Print(“Riza” == “Gilang”)
+
+#If the string on the left is equal to the string on the right, as in “banana” != “banana” , the result is False ,example:
+Print(“banana” == “banana”)
+
+#To store the result of a comparison in a variable, we use the = sign ,example:
+Fruit = “grape” != “grape”
+
+#We can also compare variables that store strings with each other, like in fruit_1 == fruit_2 ,example:
+fruit_1 = “banana”
+fruit_2 = “grape”
+print(fruit_1 == fruit_2)
+
+
