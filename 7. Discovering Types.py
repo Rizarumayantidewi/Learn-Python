e1725e50509f1a1d362b9d6ce7daccfba3601980
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-10-14 22:42:13 +0700

    Types and Comparisons 3

---
+++ 7. Discovering Types.py
@@ -0,0 +1,20 @@
+#1)	We’ve already seen a few kinds of data like numbers and strings. In programming terms, these values are called types.
+Strings are characters between quites “” ,example:
+Sugar_content = “High”
+
+#Integer is another type we’ve already used. It represents whole numbers without decimals. Example:
+Score = 25
+
+#Float is another number type we use to describe numbers with one or more digits after the decimal point. Example:
+Pie = 3.14159
+
+#The type Boolean contains only two values : the special values True and False .We’ll store the value True inside this variable. Example:
+Received_newsletter = True
+
+#When storing False in is_on ,we say that we assign a value to a variable. Example:
+Is_on = False
+
+#Assign a string to the name variable, an integer to age ,and a Boolean to is_active ,example:
+Name = “Kai”
+Age = 27
+Is_active = True
