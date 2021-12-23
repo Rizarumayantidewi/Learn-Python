6f42bc0a577f088fd095de6db8bb852a05510377
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-12-23 15:14:48 +0700

    Create Book Item.py

---
+++ Project/Book Item.py
@@ -0,0 +1,7 @@
+class Book:
+    title ="Harry Potter and the Deathly Hallows"
+    pages = 608
+    number = "8th"
+    genre = "fantasy"
+    print(f"{title} has {pages} pages")
+    print(f"It is the {number} in the series.")
