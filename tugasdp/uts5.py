b07e9e60dc53f02735285444921ea1e442514947
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2022-02-11 20:13:32 +0700

    Add files via upload

---
+++ tugasdp/uts5.py
@@ -0,0 +1,35 @@
+#Buat dictionary
+dict = {
+	'NIM' : '20200040161',
+	'Nama' : 'Riza Rumayanti Dewi',
+	'Program Studi' : 'Teknik Informatika',
+	'Hobi' : [
+		'Menari,',
+		'Membaca',
+		'Minum'
+	],
+	'Umur':'18'
+}
+
+
+#Buat Userinput mengisi variabel biodata teman
+nim = input("NIM:")
+nama = input("Nama:")
+prodi = input("Program Studi:")
+hobi = input("Hobi:")
+umur = input("Umur:") 
+print("____________________________________________")
+print(f"Nim {nim}")
+print(f"Nama {nama}")
+print(f"Prodi {prodi}")
+print(f"Hobi {hobi}")
+print(f"Umur {umur}")
+
+print("____________________________________________")
+
+#Buat Userinput mengupdate variabel biodata saya
+value1 = input("NIM:\n 20200040161")
+value2 = input("Nama:\n Riza Rumayanti Dewi")
+value3 = input("Program Studi:\n Teknik Informatika")
+value4 = input("Hobi: \n Menari \n Membaca")
+value5 = input("Umur: \n 18") 
