7e04c856d738e0c4591839dac453109124d397cd
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2022-02-11 20:11:33 +0700

    Add files via upload

---
+++ tugasdp/tugas2.py
@@ -0,0 +1,15 @@
+# Variable Nama
+Nama = input("""Nama :
+		Riza Rumayanti Dewi""")
+
+# Variable NIM
+NIM = input("""NIM : 
+		20200040161 """)
+
+#Variable Kelas
+Kelas = input("""Kelas : 
+		TI20B """)
+
+#Variable Hobi
+Hobi = input("""Hobi : 
+		Membaca dan Menari""")
