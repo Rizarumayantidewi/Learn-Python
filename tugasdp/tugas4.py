fa6c839e85ffdc46feeb8b9f01c35d7f5ceb6324
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2022-02-11 20:12:57 +0700

    Add files via upload

---
+++ tugasdp/tugas4.py
@@ -0,0 +1,19 @@
+#membuat dictionary 
+Saya = {
+	"Nama": [
+		"Riza Rumayanti Dewi"
+	],
+	"Hobi": [
+		"Membaca",
+		"Menari",
+		"Memasak",
+		"Menulis"
+	],
+	"NIM" : [
+		"20200040161"
+	],
+	"Alamat": [
+		"Cisaat"
+	]
+}
+print (Saya)
