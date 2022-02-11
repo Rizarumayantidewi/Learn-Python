f58b966cd2f25ef33459d50b7031412aeb29af41
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2022-02-11 19:47:10 +0700

    Create tugas1.py

---
+++ tugasdp/tugas1.py
@@ -0,0 +1,21 @@
+# Variable Nama
+A = "Nama saya Riza Rumayanti Dewi,"
+
+# Variable NIM
+B = " NIM saya 20200040161,"
+
+# Variable Kelas
+C = " Saya kelas TI20B,"
+
+# Variable Prodi
+D = " Program Studi Teknik Informatika"
+
+# Variable Hobi
+E = " Hobi saya Membaca dan Menari."
+
+# Gabungankan variable dan simpan di R 
+R = A + B + C + D + E
+
+# Print
+print (R)
+
